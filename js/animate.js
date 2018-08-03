48ca8aa4ad77d571658ef1f4ac0b1438a4b1629e
Keegan George <kgeorge13@gmail.com>
2018-08-03 13:25:46 -0700

    testing out animations in javascript

---
+++ js/animate.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// var aboutBtn = document.getElementById("clickable");
+// var homeContainer = document.getElementById("home-container");
+//
+// aboutBtn.addEventListener("click", function() {
+//   homeContainer.style.margin = 0;
+//
+// });
